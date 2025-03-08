a93c42fa43397462fea18c6643a0899a46a50b7a
ATVGP001 <166093079+ATVGP001@users.noreply.github.com>
2025-03-08 15:47:28 +0530

    Update index.js

--- index.js
+++ index.js
@@ -37,8 +37,8 @@ app.listen(port, () => {
   console.log('\x1b[36m[ SERVER ]\x1b[0m', '\x1b[32m SH : http://localhost:' + port + ' ✅\x1b[0m');
 });
 
-const statusMessages = ["🎧 Listening to Spotify", "🎮 Playing VALORANT"];
-const statusTypes = [ 'dnd', 'idle'];
+const statusMessages = ["Listening to 𝗧𝗶𝗰𝗸𝗲𝘁𝘀", "Playing 𝐌𝐢𝐧𝐞 𝐑𝐞𝐚𝐥𝐦𝐬"];
+const statusTypes = [ 'online', 'online'];
 let currentStatusIndex = 0;
 let currentTypeIndex = 0;
 
